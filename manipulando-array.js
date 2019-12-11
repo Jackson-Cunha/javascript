d52d2b1f675456b5ea380fc522c6c893dd1f89e5
jackson-cunha <jackson_k23@hotmail.com>
2019-12-11 14:14:33 -0300

    manipulando array 01

---
+++ manipulando-array.js
@@ -0,0 +1,29 @@
+/*manipular para o segunndo array
+*as informações do primeiro array desde que as informações repetidas venham juntas
+*restando assim no segundo array informações não repetidas.
+*/
+primeiro = ['jackson', 'jackson', 'jackson', 'joaquim', 'cunha', 'cunha', 'mars', 'jupiter']
+
+segundo = []
+
+var posicaoA = 0
+var posicaoB = 1
+var contador = 0
+var index = 0
+
+while(contador < primeiro.length) {
+    if(primeiro[posicaoA] === primeiro[posicaoB]) {
+        posicaoA++
+        posicaoB++
+    }else {
+        segundo[index] = primeiro[posicaoA]
+        index++
+        posicaoA++
+        posicaoB++
+    }
+    contador++
+}
+
+for(let i = 0; i < segundo.length; i++ ) {
+    console.log(segundo[i])
+}
