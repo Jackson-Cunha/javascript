3715165e29f17a5ba9eb97836c9fc42e05ec4541
jackson-cunha <jackson_k23@hotmail.com>
2019-12-16 11:50:31 -0300

    manipulando fila com comentários.

--- FIFO.js
+++ FIFO.js
@@ -1,17 +1,20 @@
+//valor inicial se o array estiver vazio.
 var index = -1
-
 
 array=[]
 
+//função para adicionar elementos a fila.
 function add(d) {
     array[++index] = d
 }
 
+//função que remove elementos a fila.
 function remove() {
     array[0] = null
     index--
 }
 
+//função para organizar os elemantos na fila ou seja fazer a fila andar.
 function adjustIndex() {
     let cont = 0
     for(i=1; i<= array.length; i++) {
@@ -21,6 +24,7 @@ function adjustIndex() {
     array[array.length-1] = null
 }
 
+//chamadas das funções.
 add(1)
 add(25)
 add(3)
@@ -31,5 +35,7 @@ remove()
 adjustIndex()
 add(8)
 
-
+//imprimindo os resultados.
 console.log(array)
+
+// a saida será: [ 3, 44, 8, null ]
