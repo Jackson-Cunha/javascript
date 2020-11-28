d66468b5599de21f0625bda90c0671c8c9a34b5c
Jackson Cunha <jackson_k23@hotmail.com>
2020-11-28 19:02:12 -0300

    just cleaning up
    just cleaning up

--- map.js
+++ map.js
@@ -1,6 +1,5 @@
-var arr = [{ id: 1, nome: "Antônio" }, { id: 2, nome: "José" }, { id: 3, nome: "Jonas" }, { id: 4, nome: "Carlos" }]
+var user = [{ id: 1, nome: "Antônio" }, { id: 2, nome: "José" }, { id: 3, nome: "Jonas" }, { id: 4, nome: "Carlos" }]
 
-var newarr = arr.map(function(arr) {
-    let { id, nome } = arr
-    console.log(id+" "+nome)
+user.map(u => {
+    console.log(u.id + " " + u.nome)
 })
